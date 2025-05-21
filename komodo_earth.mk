0813bb5c8e3727313c9f451f6c3847c1f7070c2f
Dtyaa <100247109+Dityay@users.noreply.github.com>
2025-05-21 16:26:01 +0800

    Add maintainer name

--- komodo_earth.mk
+++ komodo_earth.mk
@@ -32,3 +32,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     SystemName=$(PRODUCT_SYSTEM_NAME) \
     ProductModel=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+# dontol jir
+KOMODO_MAINTAINER := Pria_Tampan
